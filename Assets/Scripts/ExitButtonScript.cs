f00c27b295deb499e11a3834d5c6ed27ff1df5af
cywk <yongwankang@hotmail.com>
2021-03-18 11:41:24 +0800

    Version 2

---
+++ Assets/Scripts/ExitButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ExitButtonScript : MonoBehaviour
+{
+    //public Button bttn;
+    
+    public void BackBtt()
+    {
+        this.GetComponent<Button>().onClick.AddListener(backToGame);
+    }
+
+    public void backToGame()
+    {
+        Debug.Log("selected");
+        SceneManager.LoadScene("DifficultySelection");
+    }
+}
